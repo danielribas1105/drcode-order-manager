e00ffea71383c11d3001dc2d496979d81e6e6d72
Daniel <danielribas1105@gmail.com>
2025-03-26 23:37:38 -0300

    feat(frontend): versão inicial do frontend do projeto

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next"
+import { Montserrat, Tomorrow } from "next/font/google"
+import "./globals.css"
+
+const fontLogo = Tomorrow({
+	weight: "500",
+	style: "italic",
+	subsets: ["latin"],
+})
+
+const fontText = Montserrat({
+	subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+	title: "Order Manager",
+	description: "Gerenciador de pedidos e ordens de compra",
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="pt-BR">
+			<body className={`${fontText.className} antialiased`}>{children}</body>
+		</html>
+	)
+}
