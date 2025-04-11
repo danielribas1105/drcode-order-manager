9218f871d9a0ed8678ede319b3db835da011f653
Daniel <danielribas1105@gmail.com>
2025-04-11 19:53:00 -0300

    feat(frontend): Implementado tela de login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
 import logo from "@/../public/images/logo/logo-order-manager-400-200.png"
+import SignupForm from "@/components/ui/signup-form"
 
 export default function Login() {
 	return (
-		<div className="flex flex-col gap-3 items-center py-10 h-screen">
+		<div className="flex flex-col gap-1 items-center py-10 h-screen">
 			<Image src={logo} alt={"Logo Order Manager"} width={300} height={300} />
+			<SignupForm />
 		</div>
 	)
 }
