1bf1811037e12d7ec5f8626f13c5bcbe85723da0
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-04-17 14:29:44 -0300

    feat(frontend): exclusão das constants e hooks sem uso

--- frontend/src/data/constants/index.ts
+++
@@ -1,7 +0,0 @@
-import ordensCompra from "./ordensCompra"
-import pedidos from "./pedidos"
-import produtos from "./produtos"
-import supermercados from "./supermercados"
-import usuarios from "./usuarios"
-
-export { ordensCompra, pedidos, produtos, supermercados, usuarios }
