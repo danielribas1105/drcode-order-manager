c88a6d36e3af5a64c215bee2b970e4d5e88e342d
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-07-09 14:08:35 -0300

    bugfix: alterações para deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	// Habilitar CORS - Importante!
+	// Habilitar CORS para desenvolvimento (localhost) e produção (Vercel)
 	app.enableCors({
-		origin: ["http://localhost:3000"], // Adicione aqui a porta onde seu Next.js está rodando
+		origin: [
+			"http://localhost:3000", // desenvolvimento local
+			"https://drcode-order-manager.vercel.app", // produção na Vercel
+		],
 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		credentials: true,
+		credentials: true, // necessário se estiver usando cookies ou sessões
 	})
 
 	await app.listen(process.env.PORT ?? 4000)
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import logo from "@/../public/images/logo/logo-order-manager-400-200.png"
-/* import SignupForm from "@/components/ui/signup-form" */
+import SignupForm from "@/components/ui/signup-form"
 import Link from "next/link"
 
 export default function Login() {
 	return (
 		<div className="flex flex-col gap-1 items-center py-10 h-screen">
 			<Image src={logo} alt={"Logo Order Manager"} width={300} height={300} />
-			{/* <SignupForm /> */}
+			<SignupForm />
 			<Link href="/home">Entrar</Link>
 		</div>
 	)
